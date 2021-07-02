da4477ba8eb2a695c875957c8c613f74b405726b
s414june <s414june@gmail.com>
2021-07-02 12:08:08 +0800

    update

--- src/scroll.js
+++ src/scroll.js
@@ -1,21 +1,3 @@
-const animateCSS = (element, animation, prefix = 'animate__') =>
-    // We create a Promise and return it
-    new Promise((resolve, reject) => {
-        const animationName = `${prefix}${animation}`;
-        const node = document.querySelector(element);
-
-        node.classList.add(`${prefix}animated`, animationName);
-
-        // When the animation ends, we clean the classes and resolve the Promise
-        function handleAnimationEnd(event) {
-            event.stopPropagation();
-            node.classList.remove(`${prefix}animated`, animationName);
-            resolve('Animation ended');
-        }
-
-        node.addEventListener('animationend', handleAnimationEnd, { once: true });
-    });
-
 const animateCSSJ = (element, animation, prefix = 'animate__') =>
     // We create a Promise and return it
     new Promise((resolve, reject) => {
@@ -33,19 +15,35 @@ const animateCSSJ = (element, animation, prefix = 'animate__') =>
         node.addEventListener('animationend', handleAnimationEnd, { once: true });
     });
 
-let about = document.querySelector('#about')
-let aboutoffsetTop = [about][0].offsetTop
-let skills = document.querySelector('#skills')
-let skillsoffsetTop = [skills][0].offsetTop
-let skillsImgs = document.querySelectorAll('#skills img')
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset >= aboutoffsetTop && window.pageYOffset < aboutoffsetTop + 20) {
-        animateCSS('#about h2', 'bounce');
-        animateCSS('#about img', 'flipInX');
-    }
-    if (window.pageYOffset >= skillsoffsetTop - 20 && window.pageYOffset < skillsoffsetTop + 20) {
-        for (let i = 0; i < skillsImgs.length; i++) {
-            animateCSSJ(skillsImgs[i], 'zoomIn')
+function scrollAnimateMulti(allElement, animation) {
+    let node = document.querySelectorAll(allElement)
+    let elparent = node[0].parentElement
+    let elementOffsetTop = elparent.offsetTop
+    let elementHeight = elparent.offsetHeight
+    let winInnerHeight = window.innerHeight / 2
+    let animateOffset = elementOffsetTop - elementHeight - winInnerHeight
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset >= animateOffset && window.pageYOffset < animateOffset + 50) {
+            for (let i = 0; i < node.length; i++) {
+                animateCSSJ(node[i], animation)
+            }
         }
-    }
-})
+    })
+}
+
+function scrollAnimate(element, animation) {
+    let node = document.querySelector(element)
+    let elementOffsetTop = node.offsetTop
+    let elementHeight = node.offsetHeight
+    let winInnerHeight = window.innerHeight / 2
+    let animateOffset = elementOffsetTop - elementHeight - winInnerHeight
+    window.addEventListener('scroll', (e) => {
+        if (window.pageYOffset >= animateOffset && window.pageYOffset < animateOffset + 50) {
+            animateCSSJ(node, animation);
+        }
+    })
+}
+
+scrollAnimateMulti('#skills img', 'zoomIn')
+scrollAnimate('#about h2', 'bounce')
+scrollAnimate('#about img', 'flipInX')
