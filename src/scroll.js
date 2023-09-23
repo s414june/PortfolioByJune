7ca63202c26166b34fc266d7a57fd32b08872069
s414june <s414june@gmail.com>
2023-09-23 14:18:21 +0800

    修改過場動畫函數

--- src/scroll.js
+++ src/scroll.js
@@ -1,48 +1,38 @@
-const animateCSSJ = (element, animation, prefix = 'animate__') =>
-    // We create a Promise and return it
-    new Promise((resolve, reject) => {
-        const animationName = `${prefix}${animation}`;
-        const node = element
-        node.classList.add(`${prefix}animated`, animationName);
+const animateCSSJ = (element, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = element;
+    node.classList.add(`${prefix}animated`, animationName);
 
-        // When the animation ends, we clean the classes and resolve the Promise
-        function handleAnimationEnd(event) {
-            event.stopPropagation();
-            node.classList.remove(`${prefix}animated`, animationName);
-            resolve('Animation ended');
-        }
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
 
-        node.addEventListener('animationend', handleAnimationEnd, { once: true });
-    });
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
 
 function scrollAnimate(element, animation) {
-    let node = document.querySelector(element)
-    let elementOffsetTop = node.offsetTop
-    let elementHeight = node.offsetHeight
-    let winHeightHalf = window.innerHeight / 2
-    let animateOffset = elementOffsetTop - elementHeight - winHeightHalf
-    window.addEventListener('scroll', (e) => {
-        if (window.pageYOffset >= animateOffset && window.pageYOffset < animateOffset + 50) {
-            animateCSSJ(node, animation);
-        }
-    })
+  let nodes = document.querySelectorAll(element);
+  let winHeightHalf = window.innerHeight / 2;
+  let elementOffsetTop = nodes[0].offsetTop;
+  let elementHeight = nodes[0].offsetHeight;
+  let animateOffset = elementOffsetTop - elementHeight - winHeightHalf;
+  for (let i = 0; i < nodes.length; i++) {
+    window.addEventListener("scroll", (e) => {
+      if (
+        window.pageYOffset >= animateOffset &&
+        window.pageYOffset < animateOffset + 50
+      ) {
+        animateCSSJ(nodes[i], animation);
+      }
+    });
+  }
 }
 
-function scrollAnimateMulti(allElement, animation) {
-    let node = document.querySelectorAll(allElement)
-    let elparent = node[0].parentElement
-    let elementOffsetTop = elparent.offsetTop
-    let winHeightHalf = window.innerHeight / 2
-    let animateOffset = elementOffsetTop - winHeightHalf
-    window.addEventListener('scroll', () => {
-        if (window.pageYOffset >= animateOffset && window.pageYOffset < animateOffset + 50) {
-            for (let i = 0; i < node.length; i++) {
-                animateCSSJ(node[i], animation)
-            }
-        }
-    })
-}
-// scrollAnimateMulti('css選擇器', 'animatecss效果名稱')
-scrollAnimateMulti('#skills img', 'zoomIn')
-scrollAnimate('#about h2', 'bounce')
-scrollAnimate('#about img', 'flipInX')
+scrollAnimate("#skills img", "zoomIn");
+scrollAnimate("#about h2", "bounce");
+scrollAnimate("#about img", "flipInX");
